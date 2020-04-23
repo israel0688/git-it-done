bc1f20860e134971e8ef291d76ede17ecfd8172d
Israel Berlanga <israel0688@hotmail.com>
2020-04-23 13:02:25 -0500

    Done with 6.3

--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,3 +1,4 @@
+var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
 
 
@@ -15,6 +16,12 @@ var getRepoIssues = function(repo)
           {
             // pass response data to dom function
             displayIssues(data);
+
+            //check if api has paginated issues
+            if (response.headers.get("Link"))
+            {
+              displayWarning(repo);
+            }
           });
         }
         else {
@@ -66,3 +73,17 @@ var displayIssues = function(issues)
 
 
 };
+
+var displayWarning = function(repo)
+{
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
